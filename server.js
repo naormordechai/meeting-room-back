a4aebbbd6b5f53949e228aae8fbd9a48c74ee1e5
unknown <naormordechaix@gmail.com>
2020-02-16 23:14:08 +0200

    added production env

--- server.js
+++ server.js
@@ -16,4 +16,15 @@ app.use(bodyParser.json());
 userRoutes(app);
 roomRoutes(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('./build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+};
+
 app.listen(PORT, () => console.log(`App is listing to port ${PORT}`));
