58b90ecb3da528614e71289360b0c7e39a624dbe
unknown <naormordechaix@gmail.com>
2020-02-16 23:11:56 +0200

    new build

---
+++ build/precache-manifest.ee5b3862231d71ab3f2d07f18ec3caa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed757bd1f3e7caf0fbe4d88f8b453926",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a483b079c66c650bed1d",
+    "url": "/static/css/main.10e86d31.chunk.css"
+  },
+  {
+    "revision": "efd6da07479cc21acc03",
+    "url": "/static/js/2.3bc119ba.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3bc119ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a483b079c66c650bed1d",
+    "url": "/static/js/main.a4c65e77.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
